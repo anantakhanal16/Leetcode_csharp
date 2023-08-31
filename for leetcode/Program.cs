18ed433a83f092c7db27d9855069b8fa28e84eeb
Ananta khanal <anantaramankhanal@gmail.com>
2023-08-31 21:39:03 +0545

    for leetcode questions.

---
+++ for leetcode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Solution
+{
+   
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+       
+    }
+}
